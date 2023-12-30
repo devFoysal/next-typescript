0efc50a6f5d3cc1ad8e0d49de084d51342154dde
Foysal Mahmud <dev.foysal@gmail.com>
2023-12-31 02:06:37 +0600

    initial commit

---
+++ src/app/components/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const page = () => {
+  let abc:string = "hello ${abc} hello ${dev}";
+  let abcd = {
+    name: "hello"
+  }
+  let dd = "hello";
+  console.log("🚀 ~ file: page.tsx:9 ~ page ~ dd:", dd)
+
+  // TODO: this is should be replaced after 
+  return (
+    <div>page</div>
+  )
+}
+
+export default page
